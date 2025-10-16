c83b60950cf43e9e7270c62c2641cf6e44643b0a
Imran Shaikh <imran.shaikh@einfochips.com>
2025-10-16 08:15:42 +0530

    directoty

---
+++ Sec4RequestingUserInputs/Scripts/1.position_parameter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# echo "The first parameter is $1"
+# echo "The second parameter is $2"
+# echo "The third parameter is $3"
+
+#OR
+echo "My name is $1"
+echo "My home directory is $2"
+echo "My Favourite food is $3"
+echo "The 10th Argument is ${10}"
+echo "The 11th Argument is $11"
---
+++ Sec4RequestingUserInputs/Scripts/3.specialScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ $# -ne 2 ]]; then
+    echo "You didnt Enter Ecaxtly 2 Parameters"
+    echo "Usage: $0 <file1> <file2>"
+    ecit 1
+fi    
